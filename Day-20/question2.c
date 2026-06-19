8de8db6ac626fbdabf1e13e1537527e4017af710
25114CN208 <omprakashagrahari74@gmail.com>
2026-06-20 00:13:46 +0530

    Day-20 uploaded

---
+++ Day-20/question2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    printf("Enter size of square matrix: ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+    printf("Enter elements of matrix:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int isSymmetric = 1; 
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] != matrix[j][i]) {
+                isSymmetric = 0; 
+                break;
+            }
+        }
+        if(isSymmetric == 0) break;
+    }
+
+    if(isSymmetric == 1)
+        printf("Matrix is Symmetric.\n");
+    else
+        printf("Matrix is NOT Symmetric.\n");
+
+    return 0;
+}
